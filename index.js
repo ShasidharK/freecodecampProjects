1dc1a5e4d8a6dbc2e9993ea2726065cbe9b37716
Shasidhar Kanchu <sasidharkanchu2000@gmail.com>
2025-05-29 10:15:47 +0530

    Final

--- index.js
+++ index.js
@@ -18,13 +18,40 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+  console.log("hello");
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:year-:month-:date', (req, res) => {
+    let { year, month, date } = req.params;
+    
+    let formattedDate = new Date(`${year}-${month}-${date}`);
+    
+    if (isNaN(formattedDate.getTime())) {
+        return res.status(400).json({ error: 'Invalid date format' });
+    }
 
+    res.json({
+        unix: formattedDate.getTime(),
+        utc: formattedDate.toUTCString()
+    });
+});
+
+app.get('/api/:timestamp', (req, res) => {
+    let unixTimestamp = parseInt(req.params.timestamp);
+    
+    if (isNaN(unixTimestamp)) {
+        return res.status(400).json({ error: 'Invalid timestamp' });
+    }
+
+    let date = new Date(unixTimestamp);
+    res.json({
+        unix: unixTimestamp,
+        utc: date.toUTCString()
+    });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
