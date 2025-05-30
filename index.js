6544fffe019a9ddf87861ae48a5763d5d024a65b
Shasidhar Kanchu <sasidharkanchu2000@gmail.com>
2025-05-30 13:00:54 +0530

    Done

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  var ipaddress = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  var language = req.headers["accept-language"];
+  var software = req.headers["user-agent"];
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language.split(',')[0], 
+    software: software
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
